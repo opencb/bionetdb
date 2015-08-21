037ffe3f7fe654f5f9d96102958950686bf8a52c
pfurio <pedrofurio@gmail.com>
2015-08-21 19:48:03 +0100

    Added nodes to the database.

--- bionetdb-core/src/test/java/org/opencb/bionetdb/core/neo4j/Neo4JNetworkDBAdaptorTest.java
+++ bionetdb-core/src/test/java/org/opencb/bionetdb/core/neo4j/Neo4JNetworkDBAdaptorTest.java
@@ -1,12 +1,21 @@
 package org.opencb.bionetdb.core.neo4j;
 
 import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Result;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.opencb.bionetdb.core.api.NetworkDBAdaptor;
 import org.opencb.bionetdb.core.io.BioPaxParser;
 import org.opencb.bionetdb.core.models.Network;
+import org.opencb.datastore.core.Query;
+import org.opencb.datastore.core.QueryResult;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -17,11 +26,21 @@ public class Neo4JNetworkDBAdaptorTest {
 
     @Test
     public void testInsert() throws Exception {
+        String database = "/Users/pfurio/Downloads/neo4j-community-2.2.4/data";
         BioPaxParser bioPaxParser = new BioPaxParser("L3");
         Path inputPath = Paths.get(getClass().getResource("/Saccharomyces_cerevisiae.owl.gz").toURI());
         Network network = bioPaxParser.parse(inputPath);
 
-        NetworkDBAdaptor networkDBAdaptor = new Neo4JNetworkDBAdaptor("");
+        NetworkDBAdaptor networkDBAdaptor = new Neo4JNetworkDBAdaptor(database);
         networkDBAdaptor.insert(network, null);
+
+        // Query the test inserted data
+        String query = "MATCH n RETURN n.id";
+        System.out.println(networkDBAdaptor.get(new Query("query", query), null));
+
+
+
+
     }
+
 }
