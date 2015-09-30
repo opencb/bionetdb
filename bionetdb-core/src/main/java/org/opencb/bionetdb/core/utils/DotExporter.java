475c144e059ff6780df5497adf0dec7aa16ebc67
dapregi <dperezgil89@gmail.com>
2015-09-30 16:33:42 +0200

    Basic DotExporter imlpemented

--- bionetdb-core/src/main/java/org/opencb/bionetdb/core/utils/DotExporter.java
+++ bionetdb-core/src/main/java/org/opencb/bionetdb/core/utils/DotExporter.java
@@ -3,7 +3,10 @@ package org.opencb.bionetdb.core.utils;
 import org.opencb.biodata.formats.graph.dot.Dot;
 import org.opencb.biodata.formats.graph.dot.Edge;
 import org.opencb.biodata.formats.graph.dot.Node;
+import org.opencb.bionetdb.core.models.Interaction;
 import org.opencb.bionetdb.core.models.Network;
+import org.opencb.bionetdb.core.models.PhysicalEntity;
+import org.opencb.bionetdb.core.models.*;
 
 import java.util.HashMap;
 
@@ -16,13 +19,76 @@ public class DotExporter {
 
         Dot dot = new Dot("network", true);
 
-        dot.addNode(new Node("A", new HashMap<>()));
-        dot.addNode(new Node("B", new HashMap<>()));
-        dot.addNode(new Node("C", new HashMap<>()));
+        // Creating PhysicalEntities
+        for (PhysicalEntity physicalEntity : network.getPhysicalEntities()) {
+            dot.addNode(new Node(physicalEntity.getId(), new HashMap<>()));
+        }
 
-        dot.addEdge(new Edge("A", "C"));
+        // Creating Interactions
+        for (Interaction interaction : network.getInteractions()) {
+            dot.addNode(new Node(interaction.getId(), new HashMap<>()));
+        }
 
+        // Connecting PhysicalEntities and Interactions
+        for (Interaction interaction : network.getInteractions()) {
+            switch(interaction.getType()) {
+                case REACTION:
+                    Reaction reaction = (Reaction) interaction;
+                    for (String product : reaction.getProducts()) {
+                        dot.addEdge(new Edge(reaction.getId(), product));
+                    }
+                    for (String reactant : reaction.getReactants()) {
+                        dot.addEdge(new Edge(reactant, reaction.getId()));
+                    }
+                    break;
+                case CATALYSIS:
+                    Catalysis catalysis = (Catalysis) interaction;
+                    for (String controller : catalysis.getControllers()) {
+                        dot.addEdge(new Edge(controller, catalysis.getId()));
+                    }
+                    for (String process : catalysis.getControlledProcesses()) {
+                        dot.addEdge(new Edge(catalysis.getId(), process));
+                    }
+                    break;
+                case REGULATION:
+                    Regulation regulation = (Regulation) interaction;
+                    for (String controller : regulation.getControllers()) {
+                        dot.addEdge(new Edge(controller, regulation.getId()));
+                    }
+                    for (String process : regulation.getControlledProcesses()) {
+                        dot.addEdge(new Edge(regulation.getId(), process));
+                    }
+                    break;
+                case COLOCALIZATION:
+                    // TODO
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Creating and connecting Xrefs
+        for (PhysicalEntity physicalEntity : network.getPhysicalEntities()) {
+            for (Xref xref : physicalEntity.getXrefs()) {
+                String xrefName = xref.getSource() + ":" + xref.getId();
+                dot.addNode(new Node(xrefName, new HashMap<>()));
+                dot.addEdge(new Edge(xrefName, physicalEntity.getId()));
+            }
+        }
+
+        // Creating and connecting CellularLocations
+        for (PhysicalEntity physicalEntity : network.getPhysicalEntities()) {
+            for (CellularLocation cellularLocation : physicalEntity.getCellularLocation()) {
+                dot.addNode(new Node(cellularLocation.getName(), new HashMap<>()));
+                dot.addEdge(new Edge(cellularLocation.getName(), physicalEntity.getId()));
+
+                for (Ontology ontology : cellularLocation.getOntologies()) {
+                    String ontologyName = ontology.getSource() + ":" + ontology.getId();
+                    dot.addNode(new Node(ontologyName, new HashMap<>()));
+                    dot.addEdge(new Edge(ontologyName, cellularLocation.getName()));
+                }
+            }
+        }
         return dot;
     }
-
 }
--- bionetdb-core/src/test/java/org/opencb/bionetdb/core/utils/DotExporterTest.java
+++ bionetdb-core/src/test/java/org/opencb/bionetdb/core/utils/DotExporterTest.java
@@ -1,7 +1,10 @@
 package org.opencb.bionetdb.core.utils;
 
 import org.junit.Test;
-import org.opencb.bionetdb.core.models.Network;
+import org.opencb.bionetdb.core.models.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,6 +16,69 @@ public class DotExporterTest {
     @Test
     public void testConvert() throws Exception {
 
-        System.out.println(DotExporter.convert(new Network()));
+        Network network = new Network();
+
+        // Creating PEs
+        List<PhysicalEntity> physicalEntitiesList = new ArrayList<>();
+        physicalEntitiesList.add(new Protein("A", "A", new ArrayList<>()));
+        physicalEntitiesList.add(new Protein("B", "B", new ArrayList<>()));
+        physicalEntitiesList.add(new Protein("C", "C", new ArrayList<>()));
+        physicalEntitiesList.add(new Protein("D", "D", new ArrayList<>()));
+        physicalEntitiesList.add(new Protein("E", "E", new ArrayList<>()));
+        physicalEntitiesList.add(new Protein("F", "F", new ArrayList<>()));
+        network.setPhysicalEntities(physicalEntitiesList);
+
+        // Creating Interactions
+        List<Interaction> interactionsList = new ArrayList<>();
+        Reaction reaction1 = new Reaction("ABCD", "ABCD", new ArrayList<>(), Reaction.ReactionType.REACTION);
+        Catalysis catalysis1 = new Catalysis("EABCD", "EABCD", new ArrayList<>());
+        Regulation regulation1 = new Regulation("DEABCD", "DEABCD", new ArrayList<>());
+        Reaction reaction2 = new Reaction("DF", "DF", new ArrayList<>(), Reaction.ReactionType.REACTION);
+
+        reaction1.getReactants().add("A");
+        reaction1.getReactants().add("B");
+        reaction1.getProducts().add("C");
+        reaction1.getProducts().add("D");
+
+        catalysis1.getControllers().add("E");
+        catalysis1.getControlledProcesses().add("ABCD");
+
+        regulation1.getControllers().add("D");
+        regulation1.getControlledProcesses().add("EABCD");
+
+        reaction2.getReactants().add("D");
+        reaction2.getProducts().add("F");
+
+        interactionsList.add(reaction1);
+        interactionsList.add(catalysis1);
+        interactionsList.add(regulation1);
+        interactionsList.add(reaction2);
+
+        network.setInteractions(interactionsList);
+
+        assertEquals("Dot file: ",
+                "digraph \"network\" {\n" +
+                        "\tA;\n" +
+                        "\tB;\n" +
+                        "\tDF;\n" +
+                        "\tC;\n" +
+                        "\tD;\n" +
+                        "\tE;\n" +
+                        "\tF;\n" +
+                        "\tDEABCD;\n" +
+                        "\tABCD;\n" +
+                        "\tEABCD;\n" +
+                        "\tABCD->C;\n" +
+                        "\tABCD->D;\n" +
+                        "\tA->ABCD;\n" +
+                        "\tB->ABCD;\n" +
+                        "\tE->EABCD;\n" +
+                        "\tEABCD->ABCD;\n" +
+                        "\tD->DEABCD;\n" +
+                        "\tDEABCD->EABCD;\n" +
+                        "\tDF->F;\n" +
+                        "\tD->DF;\n" +
+                        "}\n",
+                DotExporter.convert(network).toString());
     }
 }
