6a6d83619b51b3620d39a5a98158d158b1a5d6c2
imedina <igmecas@gmail.com>
2018-03-08 16:20:53 +0000

    server: add CORS filter to REST web services

---
+++ bionetdb-server/src/main/java/org/opencb/bionetdb/server/rest/CorsFilter.java
@@ -0,0 +1,34 @@
+package org.opencb.bionetdb.server.rest;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.println("Request " + request.getMethod());
+
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "GET,POST");
+        resp.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Range, Authorization");
+
+        // Just ACCEPT and REPLY OK if OPTIONS
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+        chain.doFilter(request, servletResponse);
+    }
+
+    @Override
+    public void destroy() {}
+
+}
